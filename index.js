735589cae2dff9614c8b7471ef5ea782d84f3824
carlton.ndungu@student.moringaschool.com <carlton.ndungu@student.moringaschool.com>
2025-06-06 11:52:08 +0300

    complte lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Carlton Ndungu";
+const welcomeMessage = "Welcome to Flatbook, $(Carlton Ndungu)!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, $(CARLTON NDUNGU)!";
+const shortGreeting = "Welcome, Carlton!";
